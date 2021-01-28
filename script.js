50a8b2a2646472a8e68c1568adf75d15a70da95e
LittleMagician <bthaoxaochay@gmail.com>
2021-01-27 18:36:37 -0600

    added local storage for loop that appends

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // GIVEN a weather dashboard with form inputs
 // WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history - ONLY NEED TO APPEND
+// THEN I am presented with current and future conditions for that city and that city is added to the search history - DONE
 
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index - ONLY NEED ICON
@@ -9,7 +9,7 @@
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe - DONE
 
 // WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity - NEED CARD AND APPENDED INFO
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity - NEED DATE AND ICON
 
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
@@ -36,12 +36,15 @@ $(".button").click(function(){
 
         var onecallURL = "http://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey
 
+
+
         // Ajax Call: API for rest of weather info
         $.ajax({
             url: onecallURL,
             method: "GET"
         }).then(function(onecall){
         
+
         // Get temp, humidity, wind, UV index, and icon from object
         var tempK = onecall.current.temp;
         var tempF = Math.round((tempK*(9/5))-459.67)
@@ -51,7 +54,15 @@ $(".button").click(function(){
         var icon = onecall.current.weather.id;
 
         console.log(icon);
-
+        
+        
+        // Emptying values from previous city
+        $("#city-name").empty();
+        $("#temp").empty();
+        $("#humidity").empty();
+        $("#wind-speed").empty();
+        $("#uv-index").empty();
+        
         // Attaching weather variables to HTML
         var cityName = $("#city-name").append(city);
         $(date).append(icon);
@@ -61,6 +72,8 @@ $(".button").click(function(){
 
         var UVbold = $("<p>").html(UV);
         $("#uv-index").append(UVbold);
+
+
 
         // Getting and attaching date to HTML
         var date = new Date();
@@ -75,6 +88,8 @@ $(".button").click(function(){
 
 
         
+
+
         // UV Index Indicator
         $(UVbold).attr("class", "card bg-primary mb-3");
         $(UVbold).attr("style", "width: 1rem;");
@@ -108,14 +123,16 @@ $(".button").click(function(){
 
             var figure = $("<figure>");
             $("#five-day").append(figure);
-            $(figure).attr("class", "card");
+            $(figure).attr("class", "card text-white bg-primary");
+            $(figure).attr("style", "width: 8rem;");
 
+            var dailyIcon = $("<p>").html(onecall.daily[i].weather.icon);
             var dailyTempK = onecall.daily[i].temp.day;
-            var dailyTempF = $("<p>").html(Math.round((dailyTempK*(9/5))-459.67));
+            var dailyTempF = $("<p>").html("Temp: " + (Math.round((dailyTempK*(9/5))-459.67) + 
+            " &#8457"));
+            var dailyHumidity = $("<p>").html("Humidity: " + onecall.daily[i].humidity + "%");
 
-            var dailyHumidity = $("<p>").html(onecall.daily[i].humidity);
-
-            
+            $(figure).append(dailyIcon);
             $(figure).append(dailyTempF);
             $(figure).append(dailyHumidity);
 
@@ -126,6 +143,15 @@ $(".button").click(function(){
         
 
         // Append search history and the local storage
+        var storingCity = $("#searchBox").val();
+        localStorage.setItem("storingCity", storingCity);
+
+        for (var i = 0; i < localStorage.length; i++){
+        var storedCity = localStorage.getItem("storingCity");
+        var cityTR = $("<button>");
+        var cityText = cityTR.html(storedCity);
+        $("#nav").append(cityText);
+        }
     })
 
 })
